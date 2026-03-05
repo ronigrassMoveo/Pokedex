6e5e9ea5f1cb1b0a50a602a6f8d19eff7b16f3f4
Roni Grass <roni.grass@moveo.co.il>
2026-03-05 13:41:55 +0200

    add initial pokedex project structure

---
+++ src/app/components/pokemon-card/pokemon-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  imports: [],
+  templateUrl: './pokemon-card.html',
+  styleUrl: './pokemon-card.scss',
+})
+export class PokemonCard {}
---
+++ src/app/pages/pokemon-details/pokemon-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details',
+  imports: [],
+  templateUrl: './pokemon-details.html',
+  styleUrl: './pokemon-details.scss',
+})
+export class PokemonDetails {}
