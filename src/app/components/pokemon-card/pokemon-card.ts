4eb3c96f9591daebd0d3577bb9f15d1ffc7f4dbb
Roni Grass <roni.grass@moveo.co.il>
2026-03-08 14:22:22 +0200

    refactor: reorganize project structure and pages

--- src/app/components/pokemon-card/pokemon-card.ts
+++ src/app/components/pokemon-card/pokemon-card.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PokemonBase, PokemonDetails } from '../../models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-card',
   imports: [],
   templateUrl: './pokemon-card.html',
-  styleUrl: './pokemon-card.scss',
+  styleUrls: ['./pokemon-card.scss'],
 })
-export class PokemonCard {}
+export class PokemonCardComponent {
+  @Input() pokemon!: PokemonBase | PokemonDetails;
+
+  @Input() variant: 'list' | 'details' = 'list';
+}
