40a41bc1a48e29fb573aaf260bd41f39adeb8b65
Roni Grass <roni.grass@moveo.co.il>
2026-03-10 16:05:40 +0200

    feat: integrate pokemon API with paging and details page

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PokemonCardComponent } from '../../components/pokemon-card/pokemon-card';
-import { MOCK_POKEMONS } from '../../mock-data/pokemons.mock';
 
+import { PokemonCardComponent } from '../../components/pokemon-card/pokemon-card';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonBase } from '../../models/pokemon.model';
+
+import { PAGINATION, SEARCH } from '../../constants/pokemon.constants';
 
 @Component({
   selector: 'app-home-page',
@@ -11,6 +14,51 @@ import { MOCK_POKEMONS } from '../../mock-data/pokemons.mock';
   templateUrl: './home-page.html',
   styleUrls: ['./home-page.scss'],
 })
-export class HomePage {
-    pokemons = MOCK_POKEMONS;
+export class HomePage implements OnInit {
+  private pokemonService = inject(PokemonService);
+  private cdr = inject(ChangeDetectorRef);
+
+  pokemons: PokemonBase[] = [];
+
+  currentPage = PAGINATION.FIRST_PAGE;
+  pageSize = PAGINATION.DEFAULT_PAGE_SIZE;
+  total = 0;
+
+  searchTerm = SEARCH.EMPTY_QUERY;
+
+  ngOnInit(): void {
+    this.loadPokemons(true);
+  }
+
+  loadPokemons(reset = false): void {
+    this.pokemonService
+      .getPokemons({
+        search: this.searchTerm,
+        page: this.currentPage,
+        pageSize: this.pageSize,
+      })
+      .subscribe({
+        next: (response) => {
+          this.total = response.total;
+
+          this.pokemons = reset
+            ? response.items
+            : [...this.pokemons, ...response.items];
+
+          this.cdr.detectChanges();
+        },
+        error: (error) => {
+          console.error('pokemon load error:', error);
+        },
+      });
+  }
+
+  loadMore(): void {
+    this.currentPage += 1;
+    this.loadPokemons();
+  }
+
+  get hasMore(): boolean {
+    return this.pokemons.length < this.total;
+  }
 }
